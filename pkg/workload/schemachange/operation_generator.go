2bbe63859135951400ef123806c182f9b314d402
Faizan Qazi <faizan@cockroachlabs.com>
2025-06-11 19:04:31 +0000

    workload/schemachanger: handle foreign key mutations
    Previously, the insert operation could fail on foreign key violation
    errors, if the foreign key constraint was enforce using a mutation. This
    patch detects those scenarios and turns these errors into potential
    errors.
    
    Fixes: #148070
    
    Release note: None

--- pkg/workload/schemachange/operation_generator.go
+++ pkg/workload/schemachange/operation_generator.go
@@ -3012,6 +3012,7 @@ func (og *operationGenerator) insertRow(ctx context.Context, tx pgx.Tx) (stmt *o
 	// we will need to evaluate generated expressions below.
 	hasUniqueConstraints := false
 	hasUniqueConstraintsMutations := false
+	hasForeignKeyMutations := false
 	fkViolation := false
 	if !anyInvalidInserts {
 		// Verify if the new row may violate unique constraints by checking the
@@ -3033,17 +3034,24 @@ func (og *operationGenerator) insertRow(ctx context.Context, tx pgx.Tx) (stmt *o
 		if err != nil {
 			return nil, err
 		}
-
+		// Determine if a foreign key mutation exists.
+		hasForeignKeyMutations, err = og.tableHasForeignKeyMutation(ctx, tx, tableName)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	stmt.potentialExecErrors.addAll(codesWithConditions{
 		{code: pgcode.UniqueViolation, condition: hasUniqueConstraints || hasUniqueConstraintsMutations},
-		{code: pgcode.ForeignKeyViolation, condition: fkViolation},
+		{code: pgcode.ForeignKeyViolation, condition: fkViolation || hasForeignKeyMutations},
 		{code: pgcode.NotNullViolation, condition: true},
 		{code: pgcode.CheckViolation, condition: true},
 	})
 	og.expectedCommitErrors.addAll(codesWithConditions{
-		{code: pgcode.ForeignKeyViolation, condition: fkViolation},
+		{code: pgcode.ForeignKeyViolation, condition: fkViolation && !hasForeignKeyMutations},
+	})
+	og.potentialCommitErrors.addAll(codesWithConditions{
+		{code: pgcode.ForeignKeyViolation, condition: hasForeignKeyMutations},
 	})
 
 	var formattedRows []string
