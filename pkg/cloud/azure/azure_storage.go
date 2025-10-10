d72b0dd877304777f32cc18bba072b88e75593cd
Faizan Qazi <fqazi@users.noreply.github.com>
2025-10-09 21:28:48 -0400

    tmp

--- pkg/cloud/azure/azure_storage.go
+++ pkg/cloud/azure/azure_storage.go
@@ -12,7 +12,6 @@ import (
 	"net/url"
 	"path"
 	"strings"
-	"time"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
@@ -42,18 +41,6 @@ var maxConcurrentUploadBuffers = settings.RegisterIntSetting(
 		"Each buffer can buffer up to cloudstorage.write_chunk.size of memory during an upload",
 	1,
 	settings.WithPublic)
-
-var maxRetries = settings.RegisterIntSetting(
-	settings.ApplicationLevel,
-	"cloudstorage.azure.max_retries",
-	"the maximum number of retries per Azure operation",
-	10)
-
-var tryTimeout = settings.RegisterDurationSetting(
-	settings.ApplicationLevel,
-	"cloudstorage.azure.try.timeout",
-	"the timeout for individual retry attempts in Azure operations",
-	60*time.Second)
 
 // A note on Azure authentication:
 //
@@ -236,24 +223,15 @@ func makeAzureStorage(
 	options := args.ExternalStorageOptions()
 	t, err := cloud.MakeHTTPClient(args.Settings, args.MetricsRecorder,
 		cloud.HTTPClientConfig{
-			Bucket:         dest.AzureConfig.Container,
-			Client:         options.ClientName,
-			Cloud:          "azure",
-			HttpMiddleware: args.HttpMiddleware,
+			Bucket: dest.AzureConfig.Container,
+			Client: options.ClientName,
+			Cloud:  "azure",
 		})
 	if err != nil {
 		return nil, errors.Wrap(err, "azure: unable to create transport")
 	}
 	var opts service.ClientOptions
 	opts.Transport = t
-	// Azure SDK defaults to 3 retries, which is too low to survive the 30 second
-	// brownout in TestAzureFaultInjection.
-	opts.Retry.MaxRetries = int32(maxRetries.Get(&args.Settings.SV))
-	// We occasionally see individual requests get stuck for 10+ minutes. If the
-	// source of the stuckness is transient or applies to individual
-	// connections/requests, then starting a new request after a timeout may
-	// succeed and allow the client to make forward progress.
-	opts.Retry.TryTimeout = tryTimeout.Get(&args.Settings.SV)
 
 	var azClient *service.Client
 	switch conf.Auth {
@@ -344,15 +322,6 @@ func (s *azureStorage) Writer(ctx context.Context, basename string) (io.WriteClo
 	}), nil
 }
 
-// isNotFoundErr checks if the error indicates a blob not found condition.
-func isNotFoundErr(err error) bool {
-	if err == nil {
-		return false
-	}
-	var azerr *azcore.ResponseError
-	return errors.As(err, &azerr) && azerr.ErrorCode == "BlobNotFound"
-}
-
 func (s *azureStorage) ReadFile(
 	ctx context.Context, basename string, opts cloud.ReadOptions,
 ) (_ ioctx.ReadCloserCtx, fileSize int64, _ error) {
@@ -362,8 +331,15 @@ func (s *azureStorage) ReadFile(
 	resp, err := s.getBlob(basename).DownloadStream(ctx, &azblob.DownloadStreamOptions{Range: azblob.
 		HTTPRange{Offset: opts.Offset}})
 	if err != nil {
-		if isNotFoundErr(err) {
-			return nil, 0, cloud.WrapErrFileDoesNotExist(err, "azure blob does not exist")
+		if azerr := (*azcore.ResponseError)(nil); errors.As(err, &azerr) {
+			if azerr.ErrorCode == "BlobNotFound" {
+				// nolint:errwrap
+				return nil, 0, errors.Wrapf(
+					errors.Wrap(cloud.ErrFileDoesNotExist, "azure blob does not exist"),
+					"%v",
+					err.Error(),
+				)
+			}
 		}
 		return nil, 0, errors.Wrapf(err, "failed to create azure reader")
 	}
@@ -378,7 +354,6 @@ func (s *azureStorage) ReadFile(
 			}
 		}
 	}
-	// BUG: we should follow the azure retry setting here.
 	reader := resp.NewRetryReader(ctx, &azblob.RetryReaderOptions{MaxRetries: 3})
 	return ioctx.ReadCloserAdapter(reader), fileSize, nil
 }
@@ -415,9 +390,6 @@ func (s *azureStorage) Delete(ctx context.Context, basename string) error {
 	err := timeutil.RunWithTimeout(ctx, "delete azure file", cloud.Timeout.Get(&s.settings.SV),
 		func(ctx context.Context) error {
 			_, err := s.getBlob(basename).Delete(ctx, nil)
-			if isNotFoundErr(err) {
-				return nil
-			}
 			return err
 		})
 	return errors.Wrap(err, "delete file")
